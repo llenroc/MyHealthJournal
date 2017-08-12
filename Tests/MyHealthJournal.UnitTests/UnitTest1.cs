e3b72ef7300fe7137c9461322d58cf1347700684
jaimemorais <jaimemorais@gmail.com>
2017-08-12 16:56:32 -0300

    Add unit test project

---
+++ Tests/MyHealthJournal.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace MyHealthJournal.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int i = 1;
+
+            Assert.Equal(i, 1);
+        }
+    }
+}
